e1c6960b1636b06d5a0239123e6626b6153ce00f
Qiang Shen <johnsonzhj123@gmail.com>
2017-11-28 11:56:58 +0800

    course 1

---
+++ script/2_introduction.R
@@ -0,0 +1,22 @@
+
+## ------------------------------------------------------------------------
+ help('lm')    #"" can be omited,but not for +-*/
+ ?lm
+ example('lm') # "" can be omited
+ apropos("mean") # "" can't be omited
+ vignette("caret") #"" can't be omited
+
+## ------------------------------------------------------------------------
+search() #loaded package
+
+## ------------------------------------------------------------------------
+ls() #user-defined object
+#up/down arrow: command history
+getwd()
+list.files()
+# setwd("~/desktop") # special case for windows.
+
+## ------------------------------------------------------------------------
+if(!require(foreign)) install.packages('foreign')
+
+
